9d6a0cf503e2047104be9ed4058c30d6df76d579
Arnim Faryn <FanLin-ffms@users.noreply.github.com>
2015-10-10 02:24:32 +0200

    Supernodes aufgenommen / Namen angepasst

--- tools/gw-select/gw-select.sh
+++ tools/gw-select/gw-select.sh
@@ -5,100 +5,189 @@
 #                                                                                        #
 #                                   Nicht reboot-fest!                                   #
 #                                                                                        #
-# Aufruf: gw-select.sh fanlin|commander|fusselkater|padad0x|sense|kgbvax01|[...]|default #
+# Aufruf: gw-select.sh fanlin|commander|fusselkater|padad0x|sense|kgbvax1|[...]|default  #
 ##########################################################################################
  
  
 fanlin_only()
         {
         uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_kgbvax01.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_descilla1.enabled=0
-
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
+        }
+ 
+ fanlin1_only()
+        {
+        uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
+        }
+ 
+ fanlin2_only()
+        {
+        uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
         }
  
 commander1024_only()
         {
         uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=1
         uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_kgbvax01.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_descilla1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
         }
  
 fusselkater_only()
         {
         uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=1
         uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_kgbvax01.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_descilla1.enabled=0
-
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
         }
  
 parad0x_only()
         {
         uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=1
         uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_kgbvax01.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_descilla1.enabled=0
-
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
         }
  
 sense_only()
         {
         uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_sense.enabled=1
-        uci set fastd.mesh_vpn_backbone_peer_kgbvax01.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_descilla1.enabled=0
-
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
         }
  
-kgbvax01_only()
+kgbvax1_only()
         {
         uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_kgbvax01.enabled=1
-        uci set fastd.mesh_vpn_backbone_peer_descilla1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
         }
         
+kgbvax2a_only()
+        {
+        uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
+        }
+        
+
+kgbvax2b_only()
+        {
+        uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=0
+        }
+
 descilla1_only()
         {
         uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=0
         uci set fastd.mesh_vpn_backbone_peer_sense.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_kgbvax01.enabled=0
-        uci set fastd.mesh_vpn_backbone_peer_descilla1.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=0
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=1
         }
  
 default()
         {
         uci set fastd.mesh_vpn_backbone_peer_fanlin.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-1.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-fanlin-2.enabled=1
         uci set fastd.mesh_vpn_backbone_peer_commander1024.enabled=1
         uci set fastd.mesh_vpn_backbone_peer_fusselkater.enabled=1
         uci set fastd.mesh_vpn_backbone_peer_parad0x.enabled=1
         uci set fastd.mesh_vpn_backbone_peer_sense.enabled=1
-        uci set fastd.mesh_vpn_backbone_peer_kgbvax01.enabled=1
-        uci set fastd.mesh_vpn_backbone_peer_descilla1.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-1.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-a.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-kgbvax-2-b.enabled=1
+        uci set fastd.mesh_vpn_backbone_peer_sn-descilla-1.enabled=1
         }
  
 recycle_fastd()
@@ -114,6 +203,16 @@ recycle_fastd()
 case "$1" in
         fanlin)
                 fanlin_only
+                recycle_fastd
+                ;;
+
+        fanlin1)
+                fanlin1_only
+                recycle_fastd
+                ;;
+ 
+        fanlin2)
+                fanlin2_only
                 recycle_fastd
                 ;;
  
@@ -137,8 +236,18 @@ case "$1" in
                 recycle_fastd
                 ;;
  
-        kgbvax01)
-                kgbvax01_only
+        kgbvax1)
+                kgbvax1_only
+                recycle_fastd
+                ;;
+ 
+        kgbvax2a)
+                kgbvax2a_only
+                recycle_fastd
+                ;;
+ 
+        kgbvax2b)
+                kgbvax2b_only
                 recycle_fastd
                 ;;
  
@@ -153,7 +262,7 @@ case "$1" in
                 ;;
  
         *)
-                echo "Usage: $0 {fanlin|commander|fusselkater|parad0x|sense|kgbvax01|descilla1|default}"
+                echo "Usage: $0 {fanlin|fanlin1|fanlin2|commander|fusselkater|parad0x|sense|kgbvax1|kgbvax2a|kgbvax2b|descilla1|default}"
                 exit 1
 esac
 exit 0
